ea4f193f61a0fc55ba709e4fe7c7067ad8036aee
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-03-15 17:02:51 +0800

    feat: get headlines from news api

--- src/main/java/yangs_morning_alarm/News.java
+++ src/main/java/yangs_morning_alarm/News.java
@@ -1,5 +1,32 @@
 package yangs_morning_alarm;
 
+import java.net.URI;
+import java.net.http.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class News {
-    
+    public static void main(String[] args) throws Exception {
+        String url = "https://yangs-morning-alarm.vercel.app/api/news";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .GET() // this is the default
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JSONArray rawHeadlinesArray = new JSONArray(response.body());
+        // convert raw headlines array into proper String[]
+        String[] headlinesArray = new String[rawHeadlinesArray.length()];
+        for (int i = 0; i < rawHeadlinesArray.length(); i++) {
+            headlinesArray[i] = rawHeadlinesArray.getString(i);
+        }
+
+        for (String headline : headlinesArray) {
+            System.out.println(headline);
+        }
+    }
 }
