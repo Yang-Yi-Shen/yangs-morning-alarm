87d29ec38de84b0f81d39e66c552b46563d0dcd9
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-03-15 17:55:09 +0800

    feat: compile weather data into weather report

--- src/main/java/yangs_morning_alarm/Main.java
+++ src/main/java/yangs_morning_alarm/Main.java
@@ -1,7 +1,21 @@
 package yangs_morning_alarm;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        new TextToSpeech("Good morning yang");
+    public static void main(String[] args) throws Exception {
+        // get weather data and unpack the function return
+        ArrayList<Object> weatherData = Weather.getWeatherData();
+        double wakeUpTemp = (double) weatherData.get(0);
+        int wakeUpHumidity = (int) weatherData.get(1);
+        double maxTemp = (double) weatherData.get(2);
+        String maxTempTime = (String) weatherData.get(3);
+        int avgHumidity = (int) weatherData.get(4);
+
+        String weatherReport = String.format(
+                "The current temperature is %.1f degrees. The current humidity is %d percent. Today's temperature will peak at %.2f degrees, at around %s o'clock. The day's average humidity will be %d percent.",
+                wakeUpTemp, wakeUpHumidity, maxTemp, maxTempTime, avgHumidity);
+
+        new TextToSpeech(weatherReport);
     }
 }
