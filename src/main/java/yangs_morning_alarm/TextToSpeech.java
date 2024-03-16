3de869adda2b88856996c45b43998433d58460e8
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-03-16 17:07:58 +0800

    feat: save produced audio into .wav file

--- src/main/java/yangs_morning_alarm/TextToSpeech.java
+++ src/main/java/yangs_morning_alarm/TextToSpeech.java
@@ -2,6 +2,9 @@ package yangs_morning_alarm;
 
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.audio.SingleFileAudioPlayer;
+
+import javax.sound.sampled.AudioFileFormat;
 
 public class TextToSpeech {
     // CREDIT: STACKOVERFLOW
@@ -11,13 +14,21 @@ public class TextToSpeech {
     public TextToSpeech(String words) {
         System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
         voice = VoiceManager.getInstance().getVoice("kevin16");
+        SingleFileAudioPlayer audioPlayer = new SingleFileAudioPlayer("morning-alarm", AudioFileFormat.Type.WAVE);
         if (voice != null) {
             voice.allocate();// Allocating Voice
             try {
                 voice.setRate(165);// Setting the rate of the voice
                 voice.setPitch(90);// Setting the Pitch of the voice
                 voice.setVolume(3);// Setting the volume of the voice
-                SpeakText(words);// Calling speak() method
+
+                // save tts sound output to a .wav file
+                voice.setAudioPlayer(audioPlayer);
+                voice.allocate();
+                voice.speak(words);
+                voice.deallocate();
+
+                audioPlayer.close();
 
             } catch (Exception e1) {
                 e1.printStackTrace();
